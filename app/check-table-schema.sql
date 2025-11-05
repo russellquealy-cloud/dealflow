1a7082a91dc2b1ec412c7268e9a39fd2b9112fb5
Russell <russellquealy@gmail.com>
2025-11-04 19:06:07 -0700

    Fix build errors: november pre deploy testing

--- app/check-table-schema.sql
+++
@@ -1,7 +0,0 @@
--- Check what columns actually exist in your listings table
--- Run this first to see what columns are available
-
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns 
-WHERE table_name = 'listings' 
-ORDER BY ordinal_position;
